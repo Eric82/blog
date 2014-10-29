0a44240d5c0eda0f1fe554be8b7939ab0d6d2d98
Eric Martinez <martinez_eric82@yahoo.com>
2014-10-28 21:44:52 -0700

    pushing my php blog to github

---
+++ controller/create-db.php
@@ -0,0 +1,23 @@
+<?php
+//this calls and gets the information that is stored in database.php
+//the dir concantinates the path to the file
+    require_once (__DIR__ . "/../model/database.php");
+    //this finds the code and checks if its there and connects with the server mysqli
+    $connection = new mysqli($host, $username, $password);
+    //this code checks if the connection was successful.
+    if($connection->connect_error){
+        die("Error:" . $connection->connect_error);
+    }
+    //this checks if its true or not true and thats why its called $exists
+    $exists = $connection->select_db($database); 
+    //in this line it checkes if the connection went through and if it successed
+    if(!$exists){
+        //this is creating the database file 
+        $query = $connection->query("CREATE DATABASE $database");
+        //this wants to output a message.
+        if($query){
+            echo "Successfully created database: " . $database;
+        }
+    }
+    
+    $connection->close();
---
+++ model/database.php
@@ -0,0 +1,6 @@
+<?php
+//this is the information we use to log in phpmyadmin
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "blog_db";
