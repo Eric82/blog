d2df19a9d125ca81f1998981b48b33a495337a8e
Eric Martinez <martinez_eric82@yahoo.com>
2014-11-03 10:37:31 -0800

    adding header and footer.

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            require_once (__DIR__ . "/controller/create-db.php");
-        ?>
-    </body>
-</html>
+<?php
+    require_once (__DIR__ . "/view/header.php");
+    require_once (__DIR__ . "/controller/create-db.php");
+    require_once (__DIR__ . "/view/footer.php");
+?>
