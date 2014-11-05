0cb123fde09ce41f9e38b93f1dbc395b0f647023
Eric Martinez <martinez_eric82@yahoo.com>
2014-11-05 08:58:07 -0800

    making input and text box

---
+++ view/form.php
@@ -0,0 +1,16 @@
+<?php
+//this code will get information and let us know whats in the table that I made in create-db.php
+// textarea allows me to make as much as sentences as i want to put and do.
+?>
+<h1>Create Blog Post</h1>
+
+<form>
+    <div>
+        <laber for="title">Title:</laber>
+        <input type="text" name="title" />
+    </div>
+        <label for="post">Post:</label>
+        <textarea name="post"></textarea>
+    <div>
+    </div>
+</form>
