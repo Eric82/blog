7b5aed5233293d7e9807cf64c9e382d39eaa65bd
Eric Martinez <martinez_eric82@yahoo.com>
2014-11-12 08:57:40 -0800

    added more comments.

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,4 +1,5 @@
 <?php
+    //i need this because its trying to connect to the information in the database.
     require_once(__DIR__ . "/../mode/database.php");
 
     $connection = new mysqli($host, $username, $password, $database);
