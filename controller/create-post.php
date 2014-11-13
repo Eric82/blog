4a3489c91d9a2826a1f835ca93589f35e64fde97
Eric Martinez <martinez_eric82@yahoo.com>
2014-11-13 09:12:23 -0800

    Query our Database to submit a Blog Post

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,14 +1,21 @@
 <?php
     //i need this because its trying to connect to the information in the database.
-    require_once(__DIR__ . "/../mode/database.php");
-
+    require_once(__DIR__ . "/../model/database.php");
+    
     $connection = new mysqli($host, $username, $password, $database);
     //this post lets me know what getting posted on the webpage.
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
     //this receives the post.
-    echo "<p>Title: $title</p>";
-    echo "<p>Post: $post</p>";
+    // this will also insert something onto the table on php myadmin
+    $query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+    
+    if($query) {
+        echo "<p>Successfully inserted post: $title</p>";
+    } 
+    else {
+        echo "<p>$connection->error</p>";
+    }
     
     $connection->close();
     
