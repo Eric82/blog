638bc02ea078aeba7e3f3d5c4cae0f0b0d5e7078
Eric Martinez <martinez_eric82@yahoo.com>
2014-11-17 10:24:04 -0800

    created functions within our database class

--- model/Database.php
+++ model/Database.php
@@ -1,1 +1,10 @@
 <?php
+//stores all the variables of the code thats repeated.
+//a class allows you to do things with multiple variables and objects. It gets the information form line 12.
+class Database {
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+}
