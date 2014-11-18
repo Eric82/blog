d1194214bc8ebd34c1f6293aa4d5c3514c054dc7
Eric Martinez <martinez_eric82@yahoo.com>
2014-11-18 08:50:56 -0800

    video 30
    implementing the openConnection Function

--- model/Database.php
+++ model/Database.php
@@ -1,30 +1,39 @@
 <?php
+
 //stores all the variables of the code thats repeated.
 //a class allows you to do things with multiple variables and objects. It gets the information form line 12.
 class Database {
+
     private $connection;
     private $host;
     private $username;
     private $password;
     private $database;
+
     //this will call a function so that it grabs the information from create-db
     public function __construct($host, $username, $password, $database) {
-    //this will access the global variables
+        //this will access the global variables
         $this->host = $host;
         $this->username = $username;
         $this->password = $password;
         $this->database = $database;
     }
-    
-    public function openConnection(){
-        
+
+    public function openConnection() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        //this code checks if the connection was successful.
+        if ($this->connection->connect_error) {
+            die("Error:" . $this->connection->connect_error);
+        }
     }
-    
+
     public function closeConnection() {
         
     }
-    
-    public function query($string){
+
+    public function query($string) {
         
     }
- }
+
+}
