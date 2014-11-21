63cc0a1b86838e990de7cd9f237a34febc619c3c
Eric Martinez <martinez_eric82@yahoo.com>
2014-11-21 09:06:09 -0800

    Using Sessions to store data

--- model/config.php
+++ model/config.php
@@ -1,10 +1,18 @@
 <?php
+
     require_once (__DIR__ . "/database.php");
+    session_start();
+
     $path = "/Eric-blog/";
     //this is the information we use to log in phpmyadmin
     $host = "localhost";
     $username = "root";
     $password = "root";
-    $database = "blog_db";   
+    $database = "blog_db";
+    //session is a new dataabase variable.
+    if(!isset($_SESSION["connection"])) {
+        $connection = new Database($host, $username, $password, $database);
+        $_SESSION["connection"] = $connection;
+    }
     
-    $connection = new Database($host, $username, $password, $database);
+    
