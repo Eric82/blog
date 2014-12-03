1dd6a323e3a49b3f12b5a8d46ae134d36f7fb617
Eric Martinez <martinez_eric82@yahoo.com>
2014-12-03 09:05:46 -0800

    Database Table to Store Registered Users

--- controller/create-db.php
+++ controller/create-db.php
@@ -17,3 +17,19 @@
     else{
         echo"<p>" . $_SESSION["connection"]->error . "</p>";
     }
+    //THIS POST TABLE IS FOR OUR USERS AND THEY CAN PUT THERE INFORMATION ACCORDING TO THE TABLE.
+    $query = $_SESSION["connection"]->query("CREATE TABLE users ("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            . "username varchar (30) NOT NULL,"
+            . "email varchar(50) NOT NULL,"
+            . "passwrod char (128) NOT NULL,"
+            . "salt char(128) NOT NULL,"
+            . "PRIMARY KEY (id))");
+    
+    if($query){
+        echo "<p>Successfully created table: users</p>";
+    } 
+    else {
+        echo "<p>" . $_SESSION["connection"]->error . "</p>";
+    }
+    
