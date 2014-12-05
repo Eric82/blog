03f0c518a420ed1d0a6a642c4d9387ddafbddf92
Eric Martinez <martinez_eric82@yahoo.com>
2014-12-05 09:08:15 -0800

    Attaching a Controller to the Register Form

--- view/register-form.php
+++ view/register-form.php
@@ -1,4 +1,12 @@
-<form>
+<?php
+//this let the user register and put there information down.
+//this code will get information and let us know whats in the table that I made in create-db.php
+// textarea allows me to make as much as sentences as i want to put and do.
+require_once (__DIR__ . "/../model/config.php");
+?>
+<h1>Register</h1>
+
+<form method="post" action="<?php echo $path ."controller/create-user.php"?>;
     <div>    
         <label for="email">Email: </label>
         <input type="text" name="email"/>
