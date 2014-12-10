b6d8c95230c782d2096cce2a0215dc05a95ce6cf
Eric Martinez <martinez_eric82@yahoo.com>
2014-12-10 08:51:52 -0800

    Changing the Appearance of our date

--- controller/create-post.php
+++ controller/create-post.php
@@ -4,18 +4,21 @@
     
     //this post lets me know what getting posted on the webpage.
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
-    $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
-    $date = new DataTime('today');
-    $time = new DataTime('America/Los_Angeles');
+    $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);  
+    // $date is the date of the day we are on and will update.
+    $date = new DateTime('today');
+    // $time is the time of the locatin we are in.
+    $time = new DateTime('America/Los_Angeles');
     //this receives the post.
     // this will also insert something onto the table on php myadmin
     $query = $_SESSION["connection"]->query("INSERT INTO posts SET title = '$title', post = '$post'");
     
     if($query) {
         echo "<p>Successfully inserted post: $title</p>";
-        echo "Posted on: " . $date->format("M/D/Y"); " at " . $time->format("g:i")
+        echo "Posted on: " . $date->format("m/d/y"); " at " . $time->format("G:i");
     } 
     else {
         echo "<p>" . $_SESSION["connection"]->error . "</p>";
     }
+    
     
