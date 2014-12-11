5b3596650526f108b30fdc78f04763db3cd2d3a7
Eric Martinez <martinez_eric82@yahoo.com>
2014-12-11 08:26:25 -0800

    Storing the register form information

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,2 +1,8 @@
 <?php
-    echo "Successfully created user";
+    require_once(__DIR__ . "/../model/config.php");
+    // this is for the user to type in their information email username and password.
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+    //this will allow us to see the registering process for the user 
+    echo $email . " - " . $username . " - " . $password;
--- view/register-form.php
+++ view/register-form.php
@@ -6,7 +6,7 @@ require_once (__DIR__ . "/../model/config.php");
 ?>
 <h1>Register</h1>
 
-<form method="post" action="<?php echo $path ."controller/create-user.php"?>;
+<form method="post" action="<?php echo $path ."controller/create-user.php"?>">;
     <div>    
         <label for="email">Email: </label>
         <input type="text" name="email"/>
